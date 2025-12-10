3d372d1399b062478c069ba5360e96ad72256305
Yaponecmb <danya.sedov08@mail.ru>
2025-12-10 23:07:57 +0500

    sasd

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,9 +2,12 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    number=int(input())
+    hundreds = number // 100          
+    tens = (number // 10) % 10        
+    units = number % 10  
+    print(hundreds+tens+units)
 
-    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
